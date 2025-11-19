0b8fdc8b3d89474504519d85a9a91e322fe203f0
Praveen Mishra <103378525+Parshuu-Hub@users.noreply.github.com>
2025-11-19 13:22:36 +0530

    Rename app title from 'Medical Assistant' to 'Chatbot Assistant'

--- client/app.py
+++ client/app.py
@@ -3,8 +3,8 @@ from components.chatUI import render_chat
 from components.history_download import render_history_download
 from components.upload import render_uploader
 
-st.set_page_config(page_title="AI Medical Assistant",layout="wide")
-st.title("⚕️ Medical Assistant Chatbot")
+st.set_page_config(page_title="AI Assistant",layout="wide")
+st.title("Chatbot Assistant")
 
 render_uploader()
 render_chat()
