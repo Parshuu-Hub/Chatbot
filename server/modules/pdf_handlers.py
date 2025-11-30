8f79abfafe2f13f2604af9d9c302dfea542b8a97
Praveen Mishra <103378525+Parshuu-Hub@users.noreply.github.com>
2025-11-30 19:33:17 +0530

    Change file saving method for uploaded PDFs
    Refactor file handling to save uploaded files directly to a specified directory instead of using temporary files.

--- server/modules/pdf_handlers.py
+++ server/modules/pdf_handlers.py
@@ -1,20 +1,38 @@
+# import os
+# import shutil
+# from fastapi import UploadFile
+# import tempfile
+# from typing import List
+
+# def save_uploaded_files_temp(files: List[UploadFile]) -> List[str]:
+#     """
+#     Saves uploaded files as temporary files and returns a list of their temp file paths.
+#     The files are deleted automatically when closed unless otherwise specified.
+#     """
+#     temp_file_paths = []
+#     for file in files:
+#         # Create a NamedTemporaryFile for each uploaded file
+#         temp_file = tempfile.NamedTemporaryFile(suffix=".pdf", delete=False)
+#         temp_file.write(file.file.read())
+#         temp_file.flush()
+#         temp_file_paths.append(temp_file.name)
+#         temp_file.close()
+#     return temp_file_paths
+
+
 import os
 import shutil
 from fastapi import UploadFile
-import tempfile
-from typing import List
 
-def save_uploaded_files_temp(files: List[UploadFile]) -> List[str]:
-    """
-    Saves uploaded files as temporary files and returns a list of their temp file paths.
-    The files are deleted automatically when closed unless otherwise specified.
-    """
-    temp_file_paths = []
+
+UPLOAD_DIR="./uploaded_docs"
+
+def save_uploaded_files(files:list[UploadFile])->list[str]:
+    os.makedirs(UPLOAD_DIR,exist_ok=True)
+    file_path=[]
     for file in files:
-        # Create a NamedTemporaryFile for each uploaded file
-        temp_file = tempfile.NamedTemporaryFile(suffix=".pdf", delete=False)
-        temp_file.write(file.file.read())
-        temp_file.flush()
-        temp_file_paths.append(temp_file.name)
-        temp_file.close()
-    return temp_file_paths
+        temp_path=os.path.join(UPLOAD_DIR,file.filename)
+        with open(temp_path,"wb") as f:
+            shutil.copyfileobj(file.file,f)
+        file_path.append(temp_path)
+    return file_path
